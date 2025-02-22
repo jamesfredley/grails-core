8184a0e303abb61e18f4194cb1bb72b8599ad1ee
Jonas Pammer <opensource@jonaspammer.at>
2025-02-22 21:25:01 +0100

    fix: fix log message

--- src/testFixtures/groovy/grails/plugin/geb/GrailsGebSettings.groovy
+++ src/testFixtures/groovy/grails/plugin/geb/GrailsGebSettings.groovy
@@ -70,7 +70,9 @@ class GrailsGebSettings {
 
         File recordingDirectory = new File("${recordingDirectoryName}${File.separator}${DateTimeFormatter.ofPattern('yyyyMMdd_HHmmss').format(startTime)}")
         if (!recordingDirectory.exists()) {
-            log.info('Could not find `{}` Directory for recording. Creating...', recordingDirectoryName)
+            if (!recordingDirectory.parentFile.exists()) {
+                log.info('Could not find `{}` Directory for recording. Creating...', recordingDirectoryName)
+            }
             recordingDirectory.mkdirs()
         } else if (!recordingDirectory.isDirectory()) {
             throw new IllegalStateException("Configured recording directory '${recordingDirectory}' is expected to be a directory, but found file instead.")
@@ -87,7 +89,9 @@ class GrailsGebSettings {
 
         File reportingDirectory = new File("${reportingDirectoryName}${File.separator}${DateTimeFormatter.ofPattern('yyyyMMdd_HHmmss').format(startTime)}")
         if (!reportingDirectory.exists()) {
-            log.info('Could not find `{}` Directory for reporting. Creating...', reportingDirectoryName)
+            if (!reportingDirectory.parentFile.exists()) {
+                log.info('Could not find `{}` Directory for reporting. Creating...', reportingDirectoryName)
+            }
             reportingDirectory.mkdirs()
         } else if (!reportingDirectory.isDirectory()) {
             throw new IllegalStateException("Configured reporting directory '${reportingDirectory}' is expected to be a directory, but found file instead.")
