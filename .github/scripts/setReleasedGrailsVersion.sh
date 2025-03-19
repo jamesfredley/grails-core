dafe004e4e0de8cbf0e417bc4097fe853a1c8ee6
James Daugherty <jdaugherty@jdresources.net>
2025-03-19 08:37:18 -0400

    feedback: build updates

--- .github/scripts/setReleasedGrailsVersion.sh
+++ .github/scripts/setReleasedGrailsVersion.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Updating Grails Tests to Grails Version ${RELEASE_VERSION}"
+echo "Setting new version in GrailsUtilsTests.java: ${RELEASE_VERSION}"
 sed -i "s/assertEquals(\".*$/assertEquals(\"${RELEASE_VERSION}\", GrailsUtil.getGrailsVersion());/" "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
 sed -n "/assertEquals(\".*/p" "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
 git add "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
--- .github/scripts/setSnapshotGrailsVersion.sh
+++ .github/scripts/setSnapshotGrailsVersion.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Setting new Grails snapshot version"
+echo "Setting new version in GrailsUtilsTests.java: ${NEXT_VERSION}-SNAPSHOT"
 sed -i "s/assertEquals(\".*$/assertEquals(\"${NEXT_VERSION}-SNAPSHOT\", GrailsUtil.getGrailsVersion());/" "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
 sed -n "/assertEquals(\".*/p" "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
 git add "${GITHUB_WORKSPACE}/grails-core/src/test/groovy/grails/util/GrailsUtilTests.java"
