e0eef53a8968a645f1517445e5d6aba721bbcecf
James Daugherty <jdaugherty@jdresources.net>
2025-04-14 20:38:23 -0400

    Import grails-cache repo

---
+++ grails-cache/plugin/src/main/groovy/grails/plugin/cache/CachePluginConfiguration.groovy
@@ -0,0 +1,23 @@
+package grails.plugin.cache
+
+import groovy.transform.CompileStatic
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+/**
+ * Configuration properties for the cache plugin
+ *
+ * @author Graeme Rocher
+ * @author James Kleeh
+ */
+@CompileStatic
+@ConfigurationProperties(value = 'grails.cache')
+class CachePluginConfiguration {
+
+    Boolean clearAtStartup = false
+    Map<String, CacheConfig> caches = [:]
+
+    static class CacheConfig {
+        Integer maxCapacity
+    }
+}
+
