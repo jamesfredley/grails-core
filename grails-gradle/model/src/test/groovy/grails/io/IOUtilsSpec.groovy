57d667b0a4b4149a2e3768733645f75954f4f1b5
James Daugherty <jdaugherty@jdresources.net>
2025-04-15 09:38:31 -0400

    feedback - move tests up since class was moved

--- grails-gradle/model/src/test/groovy/grails/io/IOUtilsSpec.groovy
+++ grails-gradle/model/src/test/groovy/grails/io/IOUtilsSpec.groovy
@@ -1,19 +1,19 @@
 package grails.io
 
+import org.grails.io.support.Resource
 import spock.lang.Specification
-
 
 class IOUtilsSpec extends Specification{
 
     void "Test findClassResource finds a class resource"() {
         expect:
-        IOUtils.findClassResource(ResourceUtils)
-        IOUtils.findClassResource(ResourceUtils).path.contains('grails-bootstrap')
+        IOUtils.findClassResource(Resource)
+        IOUtils.findClassResource(Resource).path.contains('grails-gradle/model')
     }
 
     void "Test findJarResource finds a JAR resource"() {
         expect:
         IOUtils.findJarResource(Specification)
-        IOUtils.findJarResource(Specification).path.endsWith('spock-core-2.3-groovy-4.0.jar!/')
+        IOUtils.findJarResource(Specification).path.endsWith('spock-core-2.3-groovy-3.0.jar!/')
     }
 }
