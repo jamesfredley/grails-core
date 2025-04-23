0bda10d7605ca34d20bf3951a844c24a53ea49c9
James Daugherty <jdaugherty@jdresources.net>
2025-04-23 12:48:04 -0400

    Update license headers for Apache compliance

--- grails-datamapping-rx/src/main/groovy/org/grails/datastore/rx/collection/RxCollection.groovy
+++ grails-datamapping-rx/src/main/groovy/org/grails/datastore/rx/collection/RxCollection.groovy
@@ -16,7 +16,6 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-
 package org.grails.datastore.rx.collection
 
 import grails.gorm.rx.collection.ObservableCollection
--- grails-datamapping-rx/src/main/groovy/org/grails/datastore/rx/internal/RxDatastoreClientImplementor.groovy
+++ grails-datamapping-rx/src/main/groovy/org/grails/datastore/rx/internal/RxDatastoreClientImplementor.groovy
@@ -16,7 +16,6 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-
 package org.grails.datastore.rx.internal
 
 import grails.gorm.rx.proxy.ObservableProxy
