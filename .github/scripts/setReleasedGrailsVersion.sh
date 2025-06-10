a179de4bceb20678c5f33f27930feaae1b9bac91
James Daugherty <jdaugherty@jdresources.net>
2025-06-10 10:25:57 -0400

    Remove test update logic that's no longer relevant

--- .github/scripts/setReleasedGrailsVersion.sh
+++ .github/scripts/setReleasedGrailsVersion.sh
@@ -21,7 +21,4 @@ set -e
 
 echo "Update grailsVersion to ${GITHUB_REF:11}"
 sed -i "s/^grailsVersion.*$/grailsVersion\=${GITHUB_REF:11}/" gradle.properties
-sed -i "s/grailsVersion=${GITHUB_REF:11}-SNAPSHOT/grailsVersion\=${GITHUB_REF:11}/" grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
-echo "value=${GITHUB_REF:11}" >> $GITHUB_OUTPUT
 git add gradle.properties
-git add grails-forge-core/src/test/groovy/org/grails/forge/feature/grails/GrailsGradlePluginSpec.groovy
