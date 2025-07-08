c9d3221675826c7bccf27d1f7950de1db581025a
James Daugherty <jdaugherty@jdresources.net>
2025-07-07 20:42:03 -0400

    fix: updating grails doc plugin

---
+++ grails-gradle/docs-plugin/src/main/groovy/org/apache/grails/gradle/plugins/doc/GrailsDocGradlePlugin.groovy
@@ -0,0 +1,50 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.grails.gradle.plugins.doc
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import grails.doc.gradle.PublishGuideTask
+
+/**
+ * Adds Grails doc publishing support
+ *
+ * @author Graeme Rocher
+ * @since 3.0
+ */
+@CompileStatic
+class GrailsDocGradlePlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        def docsTask = project.tasks.register('publishGuide', PublishGuideTask)
+        docsTask.configure { PublishGuideTask it ->
+
+            File applicationYml = project.layout.projectDirectory.file('grails-app/conf/application.yml').getAsFile()
+            if (applicationYml.exists()) {
+                it.propertiesFiles.convention(project.files(applicationYml))
+            }
+
+            it.sourceDir.convention(project.layout.projectDirectory.dir('src/docs'))
+            it.targetDir.convention(project.layout.buildDirectory.dir('docs/manual'))
+            it.resourcesDir.convention(project.layout.projectDirectory.dir('src/docs'))
+        }
+    }
+}
