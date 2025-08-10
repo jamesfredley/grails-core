82bb9e0860e1808507915e18d5b5e5e0ea9816f5
James Fredley <jamesfredley@users.noreply.github.com>
2025-08-10 17:05:52 -0400

    Back to 7.0.0-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -33,7 +33,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("7.0.0-RC1", GrailsUtil.getGrailsVersion());
+        assertEquals("7.0.0-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
