a795fe4532ce91cf4e68d9fdc0a79c1f8030e5be
jamesfredley <jamesfredley@users.noreply.github.com>
2025-11-02 00:21:13 +0000

    [skip ci] Bump version to 7.0.2-SNAPSHOT

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -33,7 +33,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("7.0.1", GrailsUtil.getGrailsVersion());
+        assertEquals("7.0.2-SNAPSHOT", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
