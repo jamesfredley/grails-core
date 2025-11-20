53eb5f95b6438e94edf9ff939bd22c8adebde96b
Søren Berg Glasius <soeren@glasius.dk>
2025-11-20 08:26:11 +0100

    Fixed whitespace issues causing codeNarc errlrs

--- grails-web-url-mappings/src/main/groovy/grails/web/mapping/ResponseRedirector.groovy
+++ grails-web-url-mappings/src/main/groovy/grails/web/mapping/ResponseRedirector.groovy
@@ -93,15 +93,13 @@ class ResponseRedirector {
 
         boolean permanent = getBooleanArgument(ARGUMENT_PERMANENT, arguments)
         boolean moved = getBooleanArgument(ARGUMENT_MOVED, arguments, true)
-
-
         boolean absolute = getBooleanArgument(ARGUMENT_ABSOLUTE, arguments, true)
 
         final Map namedParameters = new LinkedHashMap<>(arguments)
         // we generate a relative link with no context path so that the absolute can be calculated by combining the serverBaseURL
         // which includes the contextPath
         // @Issue('11673')
-        if(absolute) namedParameters.put(LinkGenerator.ATTRIBUTE_CONTEXT_PATH, BLANK)
+        if (absolute) namedParameters.put(LinkGenerator.ATTRIBUTE_CONTEXT_PATH, BLANK)
 
         // If the request parameters contain "keepParamsWhenRedirect = true", then we add the original params. The
         // new attribute can be used from UrlMappings to redirect from old URLs to new ones while keeping the params
