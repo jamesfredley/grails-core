f888201b74cf465b09ffa672ca00b9f458911e97
Rahul Shishodia <rahul.shishodia.10@gmail.com>
2026-03-13 00:36:29 +0530

    added tests for explicit null

--- grails-gradle/plugins/src/test/groovy/org/grails/gradle/plugin/core/GrailsGradlePreserveParametersSpec.groovy
+++ grails-gradle/plugins/src/test/groovy/org/grails/gradle/plugin/core/GrailsGradlePreserveParametersSpec.groovy
@@ -24,6 +24,17 @@ class GrailsGradlePreserveParametersSpec extends GradleSpecification {
         result.output.contains("HAS_PRESERVE_PARAM_ENABLED=false")
     }
 
+    def "preserveParameterNames is set to true when configured as explicit null"() {
+        given:
+        setupTestResourceProject('preserve-params-null')
+
+        when:
+        def result = executeTask('inspectPreserveParam')
+
+        then:
+        result.output.contains("HAS_PRESERVE_PARAM_ENABLED=true")
+    }
+
     def "GroovyCompile tasks get parameters = true when preserveParameterNames is enabled"() {
         given:
         setupTestResourceProject('preserve-params-enabled')
