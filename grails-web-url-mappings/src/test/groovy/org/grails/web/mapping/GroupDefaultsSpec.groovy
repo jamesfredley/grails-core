c31ce04a321c2c7eff210de6118f47a1aaefd81f
Mattias Reichel <mattias.reichel@gmail.com>
2026-03-27 14:10:19 +0100

    test: simplify `GroupDefaultsSpec`

--- grails-web-url-mappings/src/test/groovy/org/grails/web/mapping/GroupDefaultsSpec.groovy
+++ grails-web-url-mappings/src/test/groovy/org/grails/web/mapping/GroupDefaultsSpec.groovy
@@ -18,113 +18,114 @@
  */
 package org.grails.web.mapping
 
-import grails.util.GrailsWebMockUtil
 import grails.web.mapping.AbstractUrlMappingsSpec
-import org.springframework.web.context.request.RequestContextHolder
 
 class GroupDefaultsSpec extends AbstractUrlMappingsSpec {
 
-    void "group defaults are inherited by child mappings"() {
-        given:
-        def holder = getUrlMappingsHolder {
-            group "/api", namespace: 'api', controller: 'resource', {
-                "/list"(action: 'list')
-                "/show"(action: 'show')
+    void 'inherits group defaults in child mappings'() {
+        given: 'a group with shared namespace and controller defaults'
+        def mappings = getUrlMappingsHolder {
+            group('/api', namespace: 'api', controller: 'resource') {
+                '/list'(action: 'list')
+                '/show'(action: 'show')
             }
         }
 
-        when:
-        def webRequest = GrailsWebMockUtil.bindMockWebRequest()
-        def infos = holder.matchAll('/api/list')
+        when: 'matching the list child mapping'
+        def matches = mappings.matchAll('/api/list')
 
-        then:
-        infos.length == 1
-        infos[0].controllerName == 'resource'
-        infos[0].namespace == 'api'
-        infos[0].actionName == 'list'
+        then: 'the list mapping inherits the group defaults'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == 'api'
+            controllerName == 'resource'
+            actionName == 'list'
+        }
 
-        when:
-        webRequest.resetParams()
-        infos = holder.matchAll('/api/show')
+        when: 'matching the show child mapping'
+        matches = mappings.matchAll('/api/show')
 
-        then:
-        infos.length == 1
-        infos[0].controllerName == 'resource'
-        infos[0].namespace == 'api'
-        infos[0].actionName == 'show'
+        then: 'the show mapping inherits the group defaults'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == 'api'
+            controllerName == 'resource'
+            actionName == 'show'
+        }
     }
 
-    void "child mappings can override group defaults"() {
-        given:
-        def holder = getUrlMappingsHolder {
-            group "/api", namespace: 'api', controller: 'defaultCtrl', {
-                "/special"(controller: 'specialCtrl', action: 'handle')
-                "/normal"(action: 'index')
+    void 'allows child mappings to override group defaults'() {
+        given: 'a group with controller defaults and an overriding child mapping'
+        def mappings = getUrlMappingsHolder {
+            group('/api', namespace: 'api', controller: 'defaultCtrl') {
+                '/special'(controller: 'specialCtrl', action: 'handle')
+                '/normal'(action: 'index')
             }
         }
 
-        when:
-        def webRequest = GrailsWebMockUtil.bindMockWebRequest()
-        def infos = holder.matchAll('/api/special')
+        when: 'matching the overriding child mapping'
+        def matches = mappings.matchAll('/api/special')
 
-        then: "the child override takes effect"
-        infos.length == 1
-        infos[0].controllerName == 'specialCtrl'
-        infos[0].namespace == 'api'
-        infos[0].actionName == 'handle'
+        then: 'the child mapping uses its explicit controller and action'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == 'api'
+            controllerName == 'specialCtrl'
+            actionName == 'handle'
+        }
 
-        when: "the child inherits the default controller"
-        webRequest.resetParams()
-        infos = holder.matchAll('/api/normal')
+        when: 'matching the child mapping without an override'
+        matches = mappings.matchAll('/api/normal')
 
-        then:
-        infos.length == 1
-        infos[0].controllerName == 'defaultCtrl'
-        infos[0].namespace == 'api'
-        infos[0].actionName == 'index'
+        then: 'the child mapping inherits the group defaults'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == 'api'
+            controllerName == 'defaultCtrl'
+            actionName == 'index'
+        }
     }
 
-    void "group without defaults still works"() {
-        given:
-        def holder = getUrlMappingsHolder {
-            group "/legacy", {
-                "/foo"(controller: 'foo', action: 'bar')
+    void 'matches groups without defaults using explicit child mappings'() {
+        given: 'a group without default namespace or controller values'
+        def mappings = getUrlMappingsHolder {
+            group('/legacy') {
+                '/foo'(controller: 'foo', action: 'bar')
             }
         }
 
-        when:
-        def webRequest = GrailsWebMockUtil.bindMockWebRequest()
-        def infos = holder.matchAll('/legacy/foo')
+        when: 'matching the explicit child mapping'
+        def matches = mappings.matchAll('/legacy/foo')
 
-        then:
-        infos.length == 1
-        infos[0].controllerName == 'foo'
-        infos[0].actionName == 'bar'
+        then: 'the explicit child mapping resolves without inherited defaults'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == null
+            controllerName == 'foo'
+            actionName == 'bar'
+        }
     }
 
-    void "nested groups inherit outer group defaults"() {
-        given:
-        def holder = getUrlMappingsHolder {
-            group "/community", namespace: 'community', {
-                group "/topics", controller: 'topic', {
-                    "/gallery"(action: 'gallery')
-                    "/create"(action: 'create')
+    void 'inherits outer group defaults across nested groups'() {
+        given: 'nested groups with defaults at different levels'
+        def mappings = getUrlMappingsHolder {
+            group('/community', namespace: 'community') {
+                group('/topics', controller: 'topic') {
+                    '/gallery'(action: 'gallery')
+                    '/create'(action: 'create')
                 }
             }
         }
 
-        when:
-        def webRequest = GrailsWebMockUtil.bindMockWebRequest()
-        def infos = holder.matchAll('/community/topics/gallery')
+        when: 'matching a child mapping inside the nested groups'
+        def matches = mappings.matchAll('/community/topics/gallery')
 
-        then:
-        infos.length == 1
-        infos[0].controllerName == 'topic'
-        infos[0].namespace == 'community'
-        infos[0].actionName == 'gallery'
-    }
-
-    void cleanup() {
-        RequestContextHolder.resetRequestAttributes()
+        then: 'the child mapping inherits defaults from the outer and inner groups'
+        matches.length == 1
+        with(matches.first()) {
+            namespace == 'community'
+            controllerName == 'topic'
+            actionName == 'gallery'
+        }
     }
 }
